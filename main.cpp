5980f657b0e613269d97334eea723933e2ae611b
Tadeusz “tadzik” Sośnierz <tadeusz.sosnierz@onet.pl>
2016-04-24 17:12:17 +0200

    Attempt to extract JSON from the hopefully HTML profile we got

--- main.cpp
+++ main.cpp
@@ -25,6 +25,24 @@ int main(int argc, char *argv[])
 
     QJsonDocument rawdata(QJsonDocument::fromJson(jsondata));
     if (!rawdata.isArray()) {
+        // perhaps we got .html in, let's try to rip JSON from that
+        QByteArray pattern("var rawData = JSON.parse('");
+        int lineStart = jsondata.indexOf(pattern);
+        if (lineStart != -1) {
+            int lineEnd = jsondata.indexOf("\n", lineStart);
+            if (lineEnd != -1) {
+                // get the actual positions of the JSON string
+                lineStart += pattern.length();
+                lineEnd   -= 3;
+                // cut the excess HTML on both sides
+                jsondata.truncate(lineEnd);
+                jsondata = jsondata.mid(lineStart);
+            }
+        }
+    }
+    rawdata = QJsonDocument::fromJson(jsondata);
+    if (!rawdata.isArray()) {
+        // if that didn't work either, we're screwed
         qDebug() << "Malformed input file";
         return 1;
     }
